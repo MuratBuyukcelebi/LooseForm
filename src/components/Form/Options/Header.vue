97be23da392fdbe08ad3567645cddf493878027b
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-05-18 17:12:21 +0300

    feat: create ItemSettings

--- src/components/Form/Options/Header.vue
+++ src/components/Form/Options/Header.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="header">
-    <div class="text-muted fs-6 fw-bold" contentEditable="true">top</div>
-    <div class="fs-1 fw-bold" contentEditable="true">Header</div>
-    <div class="fs-5 text-muted fw-lighter" contentEditable="true">description</div>
-  </div>
+  <ItemSettings>
+    <div class="header">
+      <div class="text-muted fs-6 fw-bold" contentEditable="true">top</div>
+      <div class="fs-1 fw-bold" contentEditable="true">Header</div>
+      <div class="fs-5 text-muted fw-lighter" contentEditable="true">description</div>
+    </div>
+  </ItemSettings>
 </template>
 
 <script>
+import ItemSettings from "../Settings/ItemSettings.vue";
+
 export default {
-  name: "Header"
+  name: "Header",
+  components: {
+    ItemSettings
+  }
 }
 </script>
 
---
+++ src/components/Form/Settings/ItemSettings.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="settings">
+  <slot />
+  <div class="settings__actions">
+    <button>close</button>
+    <button>Setting</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ItemSettings",
+}
+</script>
+
+<style scoped lang="scss">
+.settings {
+  transition: all 0.2s;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &__actions {
+    position: absolute;
+    right: 8px;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+    opacity: 0;
+    margin-top: auto;
+    transition: all 0.2s;
+  }
+
+  &:hover {
+    padding: 8px;
+    border: 1px solid #ccc;
+  }
+  &:hover & {
+    &__actions {
+      opacity: 1;
+    }
+  }
+}
+</style>
