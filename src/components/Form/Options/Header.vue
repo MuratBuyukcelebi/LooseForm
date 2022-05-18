131b3caed0fe3d8af1f083b852f620446f28511b
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-05-18 14:53:58 +0300

    feat: create Header component

---
+++ src/components/Form/Options/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <div class="text-muted fs-6 fw-bold" contentEditable="true">top</div>
+    <div class="fs-1 fw-bold" contentEditable="true">Header</div>
+    <div class="fs-5 text-muted fw-lighter" contentEditable="true">description</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  flex-direction: column;
+}
+</style>
