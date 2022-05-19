d1fdf15704e01c27a214e1c7ae0b33cc55276b7e
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-05-19 14:44:24 +0300

    chore: refactor code

--- src/components/Form/Options/Header.vue
+++ src/components/Form/Options/Header.vue
@@ -1,21 +1,14 @@
 <template>
-  <ItemSettings>
-    <div class="header">
-      <div class="text-muted fs-6 fw-bold" contentEditable="true">top</div>
-      <div class="fs-1 fw-bold" contentEditable="true">Header</div>
-      <div class="fs-5 text-muted fw-lighter" contentEditable="true">description</div>
-    </div>
-  </ItemSettings>
+  <div class="header">
+    <div class="text-muted fs-6 fw-bold" contentEditable="true">top</div>
+    <div class="fs-1 fw-bold" contentEditable="true">Header</div>
+    <div class="fs-5 text-muted fw-lighter" contentEditable="true">description</div>
+  </div>
 </template>
 
 <script>
-import ItemSettings from "../Settings/ItemSettings.vue";
-
 export default {
   name: "Header",
-  components: {
-    ItemSettings
-  }
 }
 </script>
 
