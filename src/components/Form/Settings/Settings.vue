880a99dc02924dc39c73ce1bd54fd553ac983e15
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-05-29 21:26:48 +0300

    feat: create size range (not done just test)

--- src/components/Form/Settings/Settings.vue
+++ src/components/Form/Settings/Settings.vue
@@ -2,13 +2,21 @@
   <div class="settings">
     <!--TEXT-->
     <div class="settings__title">Text Settings</div>
-    <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+    <input type="range" min="12" max="48" value="14" class="settings__range" @change="range">
+    <span id="rangeNumber">14</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Settings"
+  name: "Settings",
+
+  methods: {
+    range(e) {
+      this.e = e.target.value;
+      document.getElementById("rangeNumber").innerHTML = this.e;
+    }
+  },
 }
 </script>
 
@@ -27,6 +35,39 @@ export default {
     font-weight: bold;
     color: #fff;
     margin-bottom: 8px;
+  }
+  &__range {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 16px;
+    background: #fff;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+    border-radius: 8px;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 20px;
+      height: 20px;
+      background: #42b883;
+      cursor: pointer;
+      border-radius: 50%;
+    }
+
+    &::-moz-range-thumb {
+      border-radius: 50%;
+      width: 20px;
+      height: 20px;
+      background: #42b883;
+      cursor: pointer;
+    }
   }
 }
 </style>
